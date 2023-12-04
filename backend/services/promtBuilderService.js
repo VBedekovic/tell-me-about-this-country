997230539394e75b804736e19501d40111280e3f
Vedran Bedekovic <vbedekovic16@gmail.com>
2023-12-04 17:03:17 +0100

    Added first draft of the tour guide prompt builder.

---
+++ backend/services/promtBuilderService.js
@@ -0,0 +1,19 @@
+
+const tourGuideSkeletonPrompt = function (question, aboutCountry, answerRangeString="1 or 2 sentences") {
+    return `
+        You are a tour guide with the purpose to teach about the countries of the world.
+        You answer questions in ${answerRangeString}. 
+        If the question doesn't seem like a question or asks of knowledge you shouldn't know as a tour guide (like coding or math) correct the user about your purpose.
+        Write your answers like a json object with two variables "flags" and "answer".
+        Flags represents an array of string.
+        If you view the question as a valid question add "valid" in flags or invalid if not.
+        In the answer variable write what you would normally write.
+        The user is curious about ${aboutCountry} and they ask you the following question with the stated countries context in mind:
+        ${question}
+        `
+}
+
+
+console.log(tourGuideSkeletonPrompt("What are the colors of their flag?", "Spain"));
+
+
