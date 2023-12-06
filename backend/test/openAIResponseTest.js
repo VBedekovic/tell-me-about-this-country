b739dd0fca3f51d5674b0d144c7f9799d5f6e651
Vedran Bedekovic <vbedekovic16@gmail.com>
2023-12-07 00:03:44 +0100

    OpenAI API tested and working properly.

--- backend/test/openAIResponseTest.js
+++ backend/test/openAIResponseTest.js
@@ -1,12 +1,38 @@
 const { requestAIModelResponse } = require("../services/openaiApiService");
 const { tourGuideSkeletonPrompt } = require("../services/promtBuilderService")
 
-require('dotenv').config();
-
 const testAPICall = async () => {
     let result = await requestAIModelResponse(tourGuideSkeletonPrompt("What is the most famous dish from this country?", "Italy"))
-    console.log(result.data)
+    console.log(JSON.parse(result.choices[0].message.content))
 
 }
 
 testAPICall()
+
+
+/* 
+
+console.log(result)
+>>> requestAIModelResponse return object example structure
+{
+    id: 'chatcmpl-8SuzosCX5qb8bCZrnisfrSY9NfbXH',
+        object: 'chat.completion',
+        created: 1701902968,
+        model: 'gpt-3.5-turbo-0613',
+        choices: [{ index: 0, message: [Object], finish_reason: 'stop' }],
+        usage: { prompt_tokens: 167, completion_tokens: 23, total_tokens: 190 },
+    system_fingerprint: null
+}
+ */
+
+/*
+console.log(result.choices[0].message)
+>>> message object example structure
+{
+  role: 'assistant',
+  content: '{\n' +
+    '  "flags": ["valid"],\n' +
+    '  "answer": "The most famous dish from Italy is pizza."\n' +
+    '}'
+}
+*/
